ab27436cfff030e45f90aaa6470543ee04eb8e3e
Thon Ly <thonly@heartbank.ceo>
2022-12-29 21:30:00 -0800

    refactor global

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import { FRONTEND, COHORT } from "/global.mjs";
+import { FRONTEND_COURSE as COURSE, FRONTEND_COHORT as COHORT } from "https://thonly.org/global.mjs";
 
 window.onload = async () => {
     await import(`${COHORT}/components/sw-header/element.mjs`);
     await import(`${COHORT}/components/sw-footer/element.mjs`);
-    await import(`${FRONTEND}/components/sw-auth/element.mjs`);
+    await import(`${COURSE}/components/sw-auth/element.mjs`);
 
-    await import(`${FRONTEND}/components/sw-main/sw-learn/element.mjs`);
-    await import(`${FRONTEND}/components/sw-main/sw-practice/element.mjs`);
-    await import(`${FRONTEND}/components/sw-main/sw-review/element.mjs`);
-    await import(`${FRONTEND}/components/sw-main/sw-home/element.mjs`);
-    await import(`${FRONTEND}/components/sw-main/element.mjs`);
+    await import(`${COURSE}/components/sw-main/sw-learn/element.mjs`);
+    await import(`${COURSE}/components/sw-main/sw-practice/element.mjs`);
+    await import(`${COURSE}/components/sw-main/sw-review/element.mjs`);
+    await import(`${COURSE}/components/sw-main/sw-home/element.mjs`);
+    await import(`${COURSE}/components/sw-main/element.mjs`);
 
-    await import(`${FRONTEND}/components/sw-progress/element.mjs`);
-    await import(`${FRONTEND}/components/sw-music/element.mjs`);
+    await import(`${COURSE}/components/sw-progress/element.mjs`);
+    await import(`${COURSE}/components/sw-music/element.mjs`);
 
-    const { BACKGROUND, getGitHub } = await import(`${FRONTEND}/global.mjs`);
+    const { BACKGROUND, getGitHub } = await import(`${COURSE}/global.mjs`);
     if (!window.TESTING) window.clearCache();
     const github = await getGitHub();
     
