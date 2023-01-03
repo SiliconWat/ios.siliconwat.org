a21ca1bf44b01a9f5cbf5340ac621c6b8886345b
Thon Ly <thonly@heartbank.ceo>
2023-01-03 09:03:02 -0800

    refactor main

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 import { FRONTEND_COURSE as COURSE, FRONTEND_COHORT as COHORT } from "https://thonly.org/global.mjs";
 
 window.onload = async () => {
-    await import(`${COHORT}/components/sw-header/element.mjs`);
-    await import(`${COHORT}/components/sw-footer/element.mjs`);
-    await import(`${COURSE}/components/sw-auth/element.mjs`);
-
     await import(`${COURSE}/components/sw-main/sw-learn/element.mjs`);
     await import(`${COURSE}/components/sw-main/sw-practice/element.mjs`);
     await import(`${COURSE}/components/sw-main/sw-review/element.mjs`);
     await import(`${COURSE}/components/sw-main/sw-home/element.mjs`);
-    await import(`${COURSE}/components/sw-main/element.mjs`);
 
+    await import(`${COHORT}/components/sw-footer/element.mjs`);
     await import(`${COURSE}/components/sw-progress/element.mjs`);
     await import(`${COURSE}/components/sw-music/element.mjs`);
 
+    await import(`${COURSE}/components/sw-auth/element.mjs`);
+    await import(`${COHORT}/components/sw-header/element.mjs`);
+    await import(`${COURSE}/components/sw-main/element.mjs`);
+};
+
+document.querySelector('sw-main').addEventListener("sw-main", async event => {
     const { BACKGROUND, getGitHub } = await import(`${COURSE}/global.mjs`);
     if (!window.TESTING) window.clearCache();
     const github = await getGitHub();
     
-    await document.querySelector('sw-main').render(github);
+    await event.detail.component.render(github);
     document.documentElement.style.backgroundImage = BACKGROUND;
     document.body.style.display = 'flex';
     await document.querySelector('sw-header').render(github);
     await document.querySelector('sw-progress').render(github);
-};
+});
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
